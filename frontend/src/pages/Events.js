26b69dcd9a101480c77ca7d73b4ff613d54dd6d6
AliAzou01 <azouali01@gmail.com>
2024-07-24 17:15:03 +0200

    Adding a Modal

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './Events.css';
+import Modal from './components/Modal/Modal';
+import Backdrop from './components/Backdrop/Backdrop';
+
+
 
 const EventPage = () => {
-    return <h1>The EventPage </h1>;
+
+    const [creating, setCreating] = useState(false);
+
+    const createEventHandler = () => {
+        setCreating(true);
+    };
+
+    const cancelEventHandler = () => {
+        setCreating(false);
+    };
+    const confirmEventHandler = () => {
+        setCreating(false);
+    };
+
+    return (
+        <React.Fragment>
+        {creating && <Backdrop />}
+        {creating && <Modal title="Add Event" canCancel canConfirm onCancel={cancelEventHandler} onConfirm={confirmEventHandler}>
+            <p>Modal content</p>
+        </Modal>}
+        <div className="events-control">
+            <p>Share your own Events!</p>
+            <button className="btn" onClick={createEventHandler}>Create Event</button>
+        </div>
+        </React.Fragment>
+    )
 };
 
 export default EventPage;
--- frontend/src/pages/components/MainNavigation.js
+++ frontend/src/pages/components/MainNavigation.js
@@ -5,30 +5,39 @@ import './MainNavigation.css';
 
 const mainNaviagation = props => (
     <AuthContext.Consumer>
-    {context => {
-        return (
+        {context => {
+            return (
 
-        <header className='main-navigation'>
-            <div className='main-navigation-logo'>
-                <h1> EasyEvent </h1>
-            </div>
-            <nav className='main-navigation-item'>
-                <ul>
-                    {!context.token && (<li>
-                        <NavLink to="/auth">Authentification</NavLink>
-                    </li>)}
-                    {context.token  && (<li>
-                        <NavLink to="/booking">Bookings</NavLink>
-                    </li> )}
-                    
-                    
-                    <li>
-                        <NavLink to="/events">Events</NavLink>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-        )}}
+                <header className='main-navigation'>
+                    <div className='main-navigation-logo'>
+                        <h1> EasyEvent </h1>
+                    </div>
+                    <nav className='main-navigation-item'>
+                        <ul>
+                            {!context.token && (<li>
+                                <NavLink to="/auth">Authentification</NavLink>
+                            </li>)}
+                            {context.token && (
+                                <React.Fragment>
+                                    <li>
+                                        <NavLink to="/booking">Bookings</NavLink>
+                                    </li>
+                                    <li>
+                                        <button onClick={context.logout}>Logout</button> 
+                                    </li>
+                                 </React.Fragment>
+                            )}
+
+
+                            <li>
+                                <NavLink to="/events">Events</NavLink>
+                            </li>
+
+                        </ul>
+                    </nav>
+                </header>
+            )
+        }}
     </AuthContext.Consumer>
 
 );
